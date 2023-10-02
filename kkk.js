660c9a5076fec7da8ae46b10f08ce53fdd6af7a8
Rohith Chandra <bhavanamrohithchandrareddy@gmail.com>
2023-10-02 13:40:15 +0530

    tom curise

---
+++ kkk.js
@@ -0,0 +1,230 @@
+// var ll = "tomcurise"
+// console.log(ll.length)
+// var kk = "tomcurise";
+// console.log(kk.toLocaleUpperCase());
+// console.log(kk.toLocaleLowerCase());
+// var ll = "tom"
+// var mm = "curise"
+
+// console.log(ll.concat(" ").concat(mm));
+
+// var oo = " jshhf uhfhf   ";
+// console.log(oo.trim());
+// console.log(oo.charAt(4));
+// console.log(oo.charCodeAt(4))
+// var a = "tom curise";
+// console.log(a.split(" "));
+// var slice = "tom curise";
+// console.log(slice.slice(0,5));
+// var slic = "tom curise";
+// console.log(slic.substr(0,4));
+// var sli = "tom curise";
+// console.log(a.replace("o","mmm"));
+// var sl = "98"
+// console.log(sl.padEnd(19,"x"));
+// console.log(sl.padStart(19,"x"));
+
+// // TO FIND THE HIGHEST VALUE IN ARRAY
+// a = [1,23,4,5,6,7,45,8];
+// x = a[0]
+// for(i=0;i<a.length;i++){
+//   if(x<a[i]){
+// x=a[i]
+// }
+// }
+// console.log(x);
+
+
+
+// function functionname () {
+// var k = "tom curise";
+//     for(i=0;i<=10;i++){
+//         if(/[a-z]/.ig){
+
+//         }
+//     }
+// }
+// console.log()
+
+
+// var k = "rohith chandra reddy bhavanam";
+// console.log(k.match(/[a,e,i,o,u]/g));
+
+
+// var a = prompt("enter value");
+// console.log(typeof(a));
+
+// var a = "tom curise is a one of thegreastes actor is in the world of";
+// console.log(a.indexOf("is"));
+// console.log(a.lastIndexOf("is"));
+// console.log(a.match("i"));
+// console.log(a.includes("dhdbh"));
+// console.log(a.startsWith("tom"))
+// console.log(a.startsWith("fjjf"));
+// console.log(a.endsWith("tom"));
+// console.log(a.endsWith("of"));
+
+// // ARAAY
+
+// var t = [1,30,300,3,38848384,4,5,566778,9485585,];
+// console.log(t.pop())
+// console.log(t.push(2332313231313))
+// console.log(t.unshift(9222222222011111111111))
+// console.log(t)
+// console.log(t.shift())
+// console.log(t.toString());
+
+// var ttt="123_9884_83833384_83783874";
+// var aa=console.log(ttt.split("_"));
+// console.log(aa);
+// console.log(aa.join("-"));
+
+// var oo0 = [111,333,[33,3,3,3,3,3],4444,55,5,3222,1111];
+// ooo=[];
+// console.log(oo.flat());
+
+
+// var mmm = "hiii 0000 this is 0000 tom from 0000 form 0000 england"
+// console.log(mmm.replace(/0000/gi," "))
+// var t = [1,3,4,4,9,5,5,5,6,6,69999];
+// console.log(t.delete([9]));
+
+// debugger
+// for(i=0;i<=10;i++){
+//     console.log(i)
+// }
+
+// fetch({
+//     methods:"http methods",
+//     headers:{
+//            'Content-Type: 'appilication/json'
+//     },
+// })
+
+
+
+// 28-09-2023
+
+
+// FUNCTION TYPES
+// 1.normal function
+// 2.Arrow function
+// 3.anonmious function
+// 4.imdeatly invoked function
+// normal function
+// function functionname(a,b){
+//     return a,b
+// }
+// console.log(10+300);
+
+// // arrow function
+// var tom=()=>{
+//     var hello="tomcurise"
+//     return hello
+// }
+// console.log(tom());
+
+// // anonimous function
+// var hello = function(){
+//     var k = "tomcuise"
+//     return k
+// }
+// console.log(hello());
+
+// // immediatly inviked function
+
+// (function(){
+//     var kk = "the best"
+//     console.log(kk)
+// })();
+
+// // Objects:
+// var obj={
+//     vara:"tomcurise",
+//     kkk:"ramdevbaba",
+//     lll:"hello"
+// }
+
+// console.log(obj);
+// console.log(Object.keys(obj));
+// console.log(Object.values(obj));
+// console.log(Object.entries(obj));
+
+
+
+// TO MANUPULATE THE OBJECT FOR IN AND FOR OF
+
+// var iii = [1,444,333,5555,999];
+// for(aa in iii){
+//     console.log(iii);
+//     console.log(aa);
+    
+// }
+// for(aa of iii){
+//     console.log(aa)
+// }
+
+
+// for(aa in obj){
+//     console.log(obj)
+// }
+
+
+// // JSON
+
+// var mmm ={
+//     "hello":[1,4,5,5,1000],
+//     "tom curise":"hello"
+// }
+// console.log(mmm);
+// console.log(typeof(JSON.stringify(mmm)))
+// var eee = (JSON.stringify(mmm))
+// console.log(typeof(JSON.parse(eee)));
+
+
+// 29-09-2023
+
+// NUMBERS
+// to substracting the sring and Number
+// var a = 20000
+// var b= "vh"
+// console.log(a-b);
+
+// To ganarate the otp
+// var k = Math.random()
+// console.log(k.toFixed(4)*10000);
+
+
+// try,catch,finally
+
+// try{
+//     console.log(hello);
+// }catch(rr){
+//     console.log(rr);
+// }finally{
+//     console.log("tom curise");
+// }
+// debugger
+//  function functionname(a,b){
+//     try{
+//         if (b === 0) {
+//             throw new Error("uhf l hfufsf");
+
+            
+//         }
+//         return a/b;
+//     }catch(error) {
+//         console.log("hdffhgfdgfhgdhgfhgd");
+//         return NaN;
+//     }finally{
+//         console.log("tom curise is the best");
+//     }
+
+//  }
+// console.log(functionname(15,7));
+
+
+// ONCLOCK EVENT
+function hello(){
+    alert("tom curise");
+}
